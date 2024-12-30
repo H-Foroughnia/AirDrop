f98b2963feaf164edf3928dcddec5bd2e6447e17
Hamid <hamid.foroughnia@outlook.com>
2024-12-30 18:55:55 +0330

    swagger added online

--- newestSrc/EndPoints/EndPoint/Program.cs
+++ newestSrc/EndPoints/EndPoint/Program.cs
@@ -47,11 +47,8 @@ builder.Logging.AddConsole();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
