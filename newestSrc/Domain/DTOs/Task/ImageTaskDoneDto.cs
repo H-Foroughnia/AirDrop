a457370b35a82a3a2a0fa0bbd3f1633ba5dad486
Hamid <hamid.foroughnia@outlook.com>
2024-12-31 17:44:02 +0330

    imageTask_v2

--- newestSrc/Domain/DTOs/Task/ImageTaskDoneDto.cs
+++ newestSrc/Domain/DTOs/Task/ImageTaskDoneDto.cs
@@ -4,14 +4,6 @@ namespace Domain.DTOs.Task;
 
 public class ImageTaskDoneDto
 {
-    public int Id { get; set; }
-    public int UserId { get; set; }
     public int TaskId { get; set; }
     public IFormFile UploadedImage { get; set; }
-    public int StatusId { get; set; }
-    public bool Nude { get; set; } = false;
-    public bool AiValidate { get; set; } = true;
-    public string ImageHash { get; set; }
-    public DateTime Created { get; set; } = DateTime.UtcNow;
-    public DateTime Updated { get; set; } = DateTime.UtcNow;
 }
